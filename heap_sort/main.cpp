379002c2184b388ffadbb439e942c39617e55491
simplydesigner <simplydesigner@ya.ru>
2016-07-29 08:14:41 +0300

    Initial Commit

---
+++ heap_sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  heap_sort
+//
+//  Created by simplydesigner on 7/29/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
